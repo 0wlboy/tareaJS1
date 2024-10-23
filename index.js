2e3fe3eade3861a0f23a00ab00ae341d87f522b2
0wlboy <35744878+0wlboy@users.noreply.github.com>
2024-10-23 19:32:38 -0400

    update

--- index.js
+++ index.js
@@ -52,3 +52,58 @@ switch (dia){
     console.log('pon un numero de entre 1 y 7, por favor');
   break;
 }
+
+function calculadora(num1,num2,oper){
+  if (typeof(num1) === 'number' && typeof(num2)==='number'){
+    switch (oper){
+      case '+':
+        console.log(num1 + num2);
+      break;
+      case '-':
+        console.log(num1 - num2);
+      break;
+      case '*':
+        console.log(num1 * num2);
+      break;
+      case '/':
+        console.log(num1 / num2);
+      break;
+      default:
+        console.log('por favor pon un operador valido');
+      break;
+    }
+  }else{
+    console.log('por favor, introduce un numero en numero1 y numero2');
+  }
+}
+
+calculadora(1,2,'-');
+
+const display = document.getElementById("display");
+
+const number = document.querySelectorAll(".number").forEach(function (item) {
+  item.addEventListener("click", function (e) {
+    if (display.innerText === "NaN") {
+      display.innerText = "";
+    }
+    if (display.innerText === "0") {
+      display.innerText = "";
+    }
+    display.innerText += e.target.innerHTML.trim();
+  });
+})
+
+const calculate = document.querySelectorAll(".operation").forEach(function (item) {
+    item.addEventListener("click", function (e) {
+      let lastValue = display.innerText.substring(display.innerText.length, display.innerText.length - 1);
+      if(!isNaN(lastValue) && e.target.innerText === '='){
+        display.innerText = eval(display.innerText);
+      } else if (e.target.innerText === "C") {
+        display.innerText = 0;
+      }else{
+        if (!isNaN(lastValue)){
+          display.innerText += e.target.innerText;
+        }
+      }
+    });
+  });
